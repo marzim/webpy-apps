7d9d18c60e64767dbae621baf53decf603da0ca3
unknown <mc185104@WPHMC185104-00J.corp.ncr.com>
2013-12-13 17:54:58 +0800

    adding login and registration page

---
+++ blog/registration.py
@@ -0,0 +1,41 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      mc185104
+#
+# Created:     13/12/2013
+# Copyright:   (c) mc185104 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import web
+from web import form
+
+render = web.template.render('templates')
+
+vpass = form.regexp(r".{3,20}$", 'must be between 3 and 20 characters')
+vemail = form.regexp(r".*@.*", "must be a valid email address")
+
+register_form = form.Form(
+form.Textbox("username", description="Username"),
+form.Textbox("email", vemail, description="E-Mail"),
+form.Password("password", vpass, description="Password"),
+form.Password("password2", vpass, description="Repeat password"),
+form.Button("submit", type="submit", description="Register"),
+validators = [
+form.Validator("Password didn't match", lambda i: i.password == i.password2)]
+)
+
+class register:
+    def GET(self):
+        # do $:f.render() in the template
+        f = register_form()
+        return render.register(f)
+
+    def POST(self):
+        f = register_form()
+        if not f.valiates():
+            return render.register(f)
+        else:
+            # TODO: should show the home page
