a4051e1d265b21c70ea5c2534b20f19e7895daad
marzim <marvin.casagnap@gmail.com>
2014-01-10 18:25:58 +0800

    commit changes

--- blog/registration.py
+++ blog/registration.py
@@ -13,29 +13,35 @@ import web
 from web import form
 from web.template import render
 
-vpass = form.regexp(r".{3,20}$", 'must be between 3 and 20 characters')
-vemail = form.regexp(r".*@.*", "must be a valid email address")
+class Register:
+    vpass = form.regexp(r".{3,20}$", 'must be between 3 and 20 characters')
+    vemail = form.regexp(r".*@.*", "must be a valid email address")
+    vuser_req = form.Validator("Username not provided.", bool)
+    vuser_length = form.regexp(r".{3,20}$", 'must be between 3 and 20 characters')
+    vuser_exist = form.Validator("Username already exist.", lambda u: u is None or model.get_user(u.username) is None)
+    vpass_match = form.Validator("Password didn't match", lambda i: i.password == i.password2)
 
-register_form = form.Form(
-form.Textbox("username", description="Username"),
-form.Textbox("email", vemail, description="E-Mail"),
-form.Password("password", vpass, description="Password"),
-form.Password("password2", vpass, description="Repeat password"),
-form.Button("submit", type="submit", description="Register"),
-validators = [
-form.Validator("Password didn't match", lambda i: i.password == i.password2)]
-)
+    register_form = form.Form(
+    form.Textbox("username", vuser_req, vuser_length, description="Username"),
+    form.Textbox("email", vemail, description="E-Mail"),
+    form.Password("password", vpass, description="Password"),
+    form.Password("password2", vpass, description="Repeat password"),
+    form.Button("submit", type="submit", description="Register"),
+    validators = [vpass_match, vuser_exist],
+    )
 
-class register:
     def GET(self):
         # do $:f.render() in the template
-        f = register_form()
+        f = self.register_form()
         return render.registration(f)
 
     def POST(self):
-        f = register_form()
+        f = self.register_form()
+
         if not f.validates():
             return render.registration(f)
         else:
             # TODO: should show the home page
+            encrypt_pass = hashlib.sha1("sAlT754-"+f.d.password).hexdigest()
+            model.new_user(f.d.username, encrypt_pass, f.d.email)
             raise web.seeother('/')
